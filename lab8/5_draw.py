42743e82f1b91088251f0979175f12229273b3c3
Konstantinova Darya <darakonstantinova@yandex.ru>
2022-11-10 20:39:11 +0300

    Реализация появления нескольких шариков на экране

--- lab8/5_draw.py
+++ lab8/5_draw.py
@@ -1,3 +1,4 @@
+# Игра поймай шарик. Успеваешь кликнуть - зарабатываешь очки
 import pygame
 from pygame.draw import *
 from random import randint
@@ -6,6 +7,7 @@ pygame.init()
 FPS = 2
 screen = pygame.display.set_mode((1200, 900))
 
+# набор цветов
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -15,61 +17,49 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 color = COLORS[randint(0, 5)]
-x = randint(100, 101)
-y = randint(100, 101)
-r = randint(30, 31)
-dx = 1
-dy = 1
 balls = []
+# описание характеристик: количество шариков
+n = randint(3, 10)
 
 
+# характеристики шарика, описываются координаты, радиус, цвет
 def new_ball():
     global x, y, r
-    x = randint(100,700)
-    y = randint(100,500)
-    r = randint(30,50)
+    x = randint(100, 700)
+    y = randint(100, 500)
+    r = randint(30, 50)
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
     balls.append([color, (x, y), r])
     return (x, y, r, color)
 
 
+# создание n штук шариков с координатами '''
 def create_ball(n):
-    c = []
-    for i in range(n):
-        c.append((new_ball()))
-    return c
+        c = []
+        for i in range(n):
+            c.append((new_ball()))
+        return c
 
 
-
-def balls_move(c):
-    cordinates = []
-    for x, y, dx, dy, r, color in c:
-        pygame.draw.circle(screen, RED, (x,y), r)
-        if x >= 1150 or x < 50:
-            dx *= -1
-        if y >= 850 or y < 50:
-            dy *= -1
-        pygame.draw.circle(screen, color, (x+dx, y+dy), r)
-        cordinates.append((x+dx,y+dy, r, dx, dy, color))
-    return cordinates
-
-
-def click(event):
+# шарики появляются
+def click():
     print(x, y, r)
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 E = []
 points = 0
-c = create_ball(5)
+c = create_ball(n)
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
+            # проверка на попадание
             E = pygame.mouse.get_pos()
             x1 = E[0]
             y1 = E[1]
@@ -79,11 +69,9 @@ while not finished:
                 print('Количество очков', points)
             else:
                 print('не попал')
+                print('Количество очков', points)
 
-
-
-
-    new_ball()
+    c = create_ball(n)
     pygame.display.update()
     screen.fill(BLACK)
 pygame.quit()
