e4209a63f1c26653a1166eaaa21206ae965ca0f6
Konstantinova Darya <darakonstantinova@yandex.ru>
2022-11-17 17:12:14 +0300

    Реализация подсчета очков

--- lab8/6_draw.py
+++ lab8/6_draw.py
@@ -1,89 +1,50 @@
-# Игра поймай шарик. Успеваешь кликнуть - зарабатываешь очки
-import pygame
-from pygame.draw import *
-from random import randint
-pygame.init()
 
+import pygame
+import math
+from pygame.draw import *
+pygame.init()
 FPS = 2
 screen = pygame.display.set_mode((1200, 900))
 
-# набор цветов
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-MAGENTA = (255, 0, 255)
-CYAN = (0, 255, 255)
-BLACK = (0, 0, 0)
-COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-color = COLORS[randint(0, 5)]
+
 # описание характеристик: положение центра, смещение центра, количество шариков
-
-dx = 1
-dy = 1
-balls = []
-n = randint(3, 7)
-
-# характеристики шарика, описываются координаты, радиус, цвет
-def new_ball():
-    global x, y, r
-    x = randint(100, 700)
-    y = randint(100, 500)
-    r = randint(30, 50)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
-    balls.append([color, (x, y), r])
-    return (x, y, r, color)
-
-
-'''создание н штук шариков с координатами '''
-def create_ball(n):
-        c = []
-        for i in range(n):
-            c.append((new_ball()))
-        return c
+n = 6
+x = []
+y = []
+v = []
+dv = []
+r = []
+color = []
+m = []
+for i in range(n+1):
+    x.append(i * 400)
+    y.append(450)
+    r.append(i * 50)
+    dv.append(i * math.pi/4)
+    v.append(2)
+    m.append(r[i] ** 2)
+    color.append(0)
 
 
-'''движение шаров, смещение на dx и dy'''
-def balls_move(c):
-    cordinates = []
-    for x, y, dx, dy, r, color in c:
-        pygame.draw.circle(screen, RED, (x,y), r)
-        if x >= 1150 or x < 50:
-            dx *= -1
-        if y >= 850 or y < 50:
-            dy *= -1
-        pygame.draw.circle(screen, color, (x+dx, y+dy), r)
-        cordinates.append((x+dx,y+dy, r, dx, dy, color))
-    return cordinates
+color[1] = (255, 0, 0)
+color[2] = (0, 0, 255)
 
 
-def click(event):
-    print(x, y, r)
-
-pygame.display.update()
-clock = pygame.time.Clock()
-finished = False
-E = []
-points = 0
-c = create_ball(n)
-while not finished:
-    clock.tick(FPS)
+var_ = True
+while var_:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            finished = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            E = pygame.mouse.get_pos()
-            x1 = E[0]
-            y1 = E[1]
-            if ((x1 - x) ** 2 + (y1 - y) ** 2) ** 0.5 <= r:
-                print('попал')
-                points += 1
-                print('Количество очков', points)
-            else:
-                print('не попал')
+            var_ = False
 
-    c = create_ball(n)
+    for i in range(1, n+1):
+        x[i] += v[i] * math.cos(dv[i])
+        y[i] += v[i] * math.sin(dv[i])
+        pygame.draw.circle(screen, color[i], (x[i], y[i]), r[i])
+
+    pygame.time.delay(10)
     pygame.display.update()
-    screen.fill(BLACK)
 pygame.quit()
+
+
+
+
